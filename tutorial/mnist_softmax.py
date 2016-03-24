6744caf545523c041c912a893651e8e225f1d530
kkboy123 <kkboy123@users.noreply.github.com>
2016-03-24 12:37:33 +0800

    update simple minst

--- tutorial/mnist_softmax.py
+++ tutorial/mnist_softmax.py
@@ -26,26 +26,69 @@ import tensorflow as tf
 mnist = input_data.read_data_sets("data", one_hot=True)
 sess = tf.InteractiveSession()
 
-# Create the model
+'''
+Create the model
+x, which is a 784-dim vector, is input layer
+W, which is a 784x10 matrix, is the weight of the input layer
+b, which is a 784-dim vector, is the bias of the input layer
+y, which is a one-hot vector (10-dim), is the prediction
+'''
 x = tf.placeholder(tf.float32, [None, 784])
 W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x, W) + b)
 
-# Define loss and optimizer
+'''
+Define loss and optimizer
+'''
 y_ = tf.placeholder(tf.float32, [None, 10])
-cross_entropy = -tf.reduce_sum(y_ * tf.log(y))
-# train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
-train_step = tf.train.MomentumOptimizer(0.01,0.01).minimize(cross_entropy)
+'''
+cross_entropy
+'''
+loss = -tf.reduce_sum(y_ * tf.log(y))
+'''root mean square'''
+# loss = tf.reduce_mean(tf.square(y - y_))
+'''
+different training method
+https://www.tensorflow.org/versions/r0.7/api_docs/python/train.html#optimizers
+'''
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+# train_step = tf.train.AdamOptimizer(0.01).minimize(loss)
+# train_step = tf.train.MomentumOptimizer(0.01,0.01).minimize(cross_entropy)
 
-# Train
 tf.initialize_all_variables().run()
+
+'''
+Training 1000 times
+Mini batch
+batch size = 100
+'''
 for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     train_step.run({x: batch_xs, y_: batch_ys})
 
-# Test trained model
+''' Test trained model'''
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 print(accuracy.eval({x: mnist.test.images, y_: mnist.test.labels}))
 sess.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Exercise 1
+try different loss function and optimizer
+
+Exercise 2
+add another layer
+'''
